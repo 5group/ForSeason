5da3c12fb1ab7e91ebc1c66e1d3430f0369748c1
chominsoo4245 <chominsoo4245@gmail.com>
2023-01-16 00:40:13 +0900

    #1 로그인 / 회원가입 -> 카카오, 네이버 버그 발생 -> kakao 버그 수정완료.. (네이버 진행중)

--- ForSeason/src/main/java/com/shop/controller/NaverController.java
+++ ForSeason/src/main/java/com/shop/controller/NaverController.java
@@ -16,24 +16,24 @@ import com.shop.service.UserService;
 
 @Controller
 public class NaverController {
-	  	@Autowired
-	    NaverService naverService;
-	  
-	    @Autowired
-	    UserService userService;
-	   
-	    @RequestMapping("/naverlogin")
-		public String callback(Model model, String code, HttpSession session) throws Exception {
-			System.out.println("callback 호출");
-			String access_Token = naverService.getAccessToken(code);  //토큰 가져오기
-			HashMap<String, String> userInfo = naverService.getUserInfo(access_Token);
-			model.addAttribute("userObj", userInfo);
-			//유저 회원가입 확인
-			User user = userService.get_id(userInfo.get("user_id"));
-			if(user != null){
-		          session.setAttribute("loginuser", user);
-				model.addAttribute("is_check", "true");
-		    }
-		    return "oauth/firstlogin";
-		}
+    @Autowired
+    NaverService naverService;
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/naverlogin")
+    public String callback(Model model, String code, HttpSession session) throws Exception {
+        System.out.println("callback 호출");
+        String access_Token = naverService.getAccessToken(code);  //토큰 가져오기
+        HashMap<String, String> userInfo = naverService.getUserInfo(access_Token);
+        model.addAttribute("userObj", userInfo);
+        //유저 회원가입 확인
+        User user = userService.get_id(userInfo.get("user_id"));
+        if (user != null) {
+            session.setAttribute("loginuser", user);
+            model.addAttribute("is_check", "true");
+        }
+        return "oauth/firstlogin";
+    }
 }
